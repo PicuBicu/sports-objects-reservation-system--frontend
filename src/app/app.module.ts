abf61f226d6ef74849f3d73ceccb60a894631ff3
PicuBicu <championello@gmail.com>
2022-06-02 15:26:15 +0200

    Add material modules that holds all material components modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,36 +6,21 @@ import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FooterComponent } from "./common/footer/footer.component";
 import { HeaderComponent } from "./common/header/header.component";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatIconModule } from "@angular/material/icon";
+
 import { LoginComponent } from "./auth/login/login.component";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatButtonToggleModule } from "@angular/material/button-toggle";
-import { MatButtonModule } from "@angular/material/button";
 import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { ToastrModule } from "ngx-toastr";
 import { AuthInterceptor } from "./auth/auth.interceptor";
 import { RegisterComponent } from "./auth/register/register.component";
-import { MatGridListModule } from "@angular/material/grid-list";
-import { MatCardModule } from "@angular/material/card";
-import { MatStepperModule } from "@angular/material/stepper";
 import { UserControlPanelComponent } from "./user/user-control-panel/user-control-panel.component";
-import { MatTableModule } from "@angular/material/table";
-import { MatSortModule } from "@angular/material/sort";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatDividerModule } from "@angular/material/divider";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatChipsModule } from "@angular/material/chips";
-import { MatListModule } from "@angular/material/list";
-import { MatAutocompleteModule } from "@angular/material/autocomplete";
 import { UserDetailsComponent } from "./user/user-details/user-details.component";
 import { SportObjectControlPanelComponent } from "./sport-objects/sport-objects-control-panel/sport-object-control-panel.component";
 import { CreateSportObjectDialogComponent } from "./sport-objects/create-sport-object-dialog/create-sport-object-dialog.component";
 import { JwtHelperService, JwtModule, JWT_OPTIONS } from "@auth0/angular-jwt";
 import { StartComponent } from "./common/start/start.component";
+import { SportObjectsDetailsComponent } from "./sport-objects/sport-objects-details/sport-objects-details.component";
+import { MaterialModule } from "./material/material.module";
 
 export function tokenGetter() {
   return localStorage.getItem("access_token");
@@ -53,42 +38,26 @@ export function tokenGetter() {
     SportObjectControlPanelComponent,
     CreateSportObjectDialogComponent,
     StartComponent,
+    SportObjectsDetailsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatFormFieldModule,
-    MatInputModule,
     FormsModule,
     ReactiveFormsModule,
-    MatButtonToggleModule,
-    MatButtonModule,
     HttpClientModule,
     ToastrModule.forRoot({
       timeOut: 10000,
       positionClass: "toast-top-right",
       preventDuplicates: true,
     }),
-    MatGridListModule,
-    MatCardModule,
-    MatStepperModule,
-    MatTableModule,
-    MatSortModule,
-    MatPaginatorModule,
-    MatCheckboxModule,
-    MatDividerModule,
-    MatDialogModule,
-    MatListModule,
-    MatChipsModule,
-    MatAutocompleteModule,
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
       },
     }),
+    MaterialModule,
   ],
   providers: [
     {
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatButtonModule } from "@angular/material/button";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatTableModule } from "@angular/material/table";
+import { MatSortModule } from "@angular/material/sort";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatListModule } from "@angular/material/list";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+
+const MaterialModules = [
+  MatToolbarModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonToggleModule,
+  MatButtonModule,
+  MatGridListModule,
+  MatCardModule,
+  MatStepperModule,
+  MatTableModule,
+  MatSortModule,
+  MatPaginatorModule,
+  MatCheckboxModule,
+  MatDividerModule,
+  MatDialogModule,
+  MatListModule,
+  MatChipsModule,
+  MatAutocompleteModule,
+];
+
+@NgModule({
+  imports: [MaterialModules],
+  exports: [MaterialModules],
+})
+export class MaterialModule {}
